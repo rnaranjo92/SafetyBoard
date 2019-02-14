1f8b44f97eb595e474c08dbc6e3d5795b3cf68cd
Rolando Naranjo <rolando.naranjo92@yahoo.com>
2019-02-13 20:48:42 -0800

    Revert "Deleted my User Model. I only used it for practice. Now I will list of users and its information in table and admin users will be able to edit user names and provide access to users within the website so they can start posting."
    This reverts commit a9c13073ab11542a375128b3cb5efdb9024aacf4.

---
+++ SafetyBoard/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SafetyBoard.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Name is required")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribeToNewsletter { get; set; }
+
+        public UserType UserType { get; set; }
+
+        [Required(ErrorMessage = "Type of User is required")]
+        [Display(Name = "Type of User")]
+        public byte UserTypeId { get; set; }
+
+        
+        [Display(Name = "Date of Birth")]
+        public DateTime? BirthDate { get; set; }
+
+        [Display(Name = "Date Registered")]
+        public DateTime DateRegistered { get; set; }
+
+        [Required(ErrorMessage = "Organization is required")]
+        public string Organization { get; set; }
+    }
+}
---
+++ SafetyBoard/Models/ViewModel/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SafetyBoard.Models.ViewModel
+{
+    public class UserViewModel
+    {
+        public List<UserType> UserType { get; set; }
+        public User User { get; set; }
+        public string PageTitle { get; set; }
+    }
+}
