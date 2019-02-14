4ad0ba1bbeab22d820d97f2a6e037f4eaba46e34
Rolando Naranjo <rolando.naranjo92@yahoo.com>
2019-02-13 20:55:29 -0800

    Deleted my User Model. I only used it for practice. Now I will list of users and its information in table and admin users will be able to edit user names and provide access to users within the website so they can start posting. I made a mistake on clicking revert xD

--- SafetyBoard/Models/Min18YearsOld.cs
+++ SafetyBoard/Models/Min18YearsOld.cs
@@ -10,20 +10,19 @@ namespace SafetyBoard.Models
     {
         public static readonly byte LegalAge = 18;
 
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
+     //   protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+   //     {
             
-            var user = (User)validationContext.ObjectInstance;
+           // var user = (User)validationContext.ObjectInstance;
 
-            if (user.BirthDate == null)
-                return new ValidationResult("Birthdate is required.");
+          //  if (user.BirthDate == null)
+           //     return new ValidationResult("Birthdate is required.");
                 
 
-            var age = DateTime.Today.Year - user.BirthDate.Value.Year;
-
-            return (age >= LegalAge)
-                ? ValidationResult.Success
-                : new ValidationResult("Must be 18 years old of age in order to register.");
-        }
+         //   var age = DateTime.Today.Year - user.BirthDate.Value.Year;
+         //  return (age >= LegalAge)
+           //     ? ValidationResult.Success
+            //    : new ValidationResult("Must be 18 years old of age in order to register.");
+     //   }
     }
 }
