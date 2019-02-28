3ae115f12d2d497d1012190741d0105797416c6d
Rolando Naranjo <rolando.naranjo92@yahoo.com>
2019-02-27 21:58:10 -0800

    Adding "my upcoming nspection" view

--- SafetyBoard/Controllers/HomeController.cs
+++ SafetyBoard/Controllers/HomeController.cs
@@ -1,8 +1,7 @@
 ﻿using SafetyBoard.Models;
 using System;
-using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace SafetyBoard.Controllers
@@ -10,9 +9,21 @@ namespace SafetyBoard.Controllers
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var upcomingInspections = _context.Inspections
+                .Include(i => i.Inspector)
+                .Include(i=>i.InspectionType)
+                .Include(i=>i.Organization)
+                .Where(i => i.DateTime > DateTime.Now);
+
+            return View(upcomingInspections);
         }
 
         public ActionResult About()
